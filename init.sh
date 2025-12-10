92aa5bbac5795e016dbbec1b4ba8d8d46b90f37a
i <espinosa153952>
2025-12-10 20:09:49 +0100

    Modificado init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y docker docker-compose apt-transport-https ca-certificates curl software-properties-common lsb-release gnupg
-sudo systemctl status docker
 sudo usermod -aG docker $USER
