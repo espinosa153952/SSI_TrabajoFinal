2efc416f30b154c4e86ea14fc4debf77f7e8e0dc
i <espinosa153952>
2025-12-16 18:59:25 +0100

    Añadida la flag a la base de datos

--- database/machine/src/prepare.sh
+++ database/machine/src/prepare.sh
@@ -1,4 +1,4 @@
 apt update && apt install -y mariadb-server && apt clean
 sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
-FLAG=`cat db_flag.txt`
-echo "INSERT INTO facts (fact) VALUES ('$FLAG')"
+FLAG=`cat /opt/src/db_flag.txt`
+echo "INSERT INTO facts (fact) VALUES ('$FLAG');" >> "/opt/src/init.sql"
