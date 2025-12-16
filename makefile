f90ddc2a6c8611bc1042f791616f84692f47bda4
i <espinosa153952>
2025-12-16 18:28:58 +0100

    Flag añadida a la base de datos

--- makefile
+++ makefile
@@ -1,17 +1,6 @@
 main:
+	/bin/bash start.sh
 	docker-compose up --force-recreate -d
 
 clean: 
 	docker-compose down --rmi all -v
-
-dbBash:
-	docker exec -it db bash
-
-dbLogs:
-	docker logs db 
-
-serverBash:
-	docker exec -it server bash
-
-serverLogs:
-	docker logs server
--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+mkdir -p ssh/machine/flag
+mkdir -p database/machine/flag
 echo $RANDOM | md5sum  | awk '{print "ssi{"$1"}"}' > ssh/machine/flag/user_flag.txt
 echo $RANDOM | md5sum  | awk '{print "ssi{"$1"}"}' > ssh/machine/flag/root_flag.txt
 echo $RANDOM | md5sum  | awk '{print "ssi{"$1"}"}' > database/machine/flag/db_flag.txt
