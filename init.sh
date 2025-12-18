69e268f6670c718b5bb8c60c345b86f001e411de
i <espinosa153952>
2025-12-18 02:04:31 +0100

    Modificado el makefile para que no funcione con sshpass

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y docker docker-compose apt-transport-https ca-certificates curl software-properties-common lsb-release gnupg sqlmap nmap ssh sshpass
+sudo apt install -y docker docker-compose apt-transport-https ca-certificates curl software-properties-common lsb-release gnupg sqlmap nmap ssh
 sudo usermod -aG docker $USER
--- makefile
+++ makefile
@@ -9,7 +9,7 @@ init:
 	/bin/bash init.sh
 
 pivotingServer:
-	sshpass -p 'ssi' ssh ssi@localhost -L 8080:server:8080 -N -T
+	ssh ssi@localhost -L 8080:server:8080 -N -T
 
 pivotingSSH:
-	sshpass -p 'ssi' ssh ssi@localhost -L 22000:ssh:22 -N -T
+	ssh ssi@localhost -L 22000:ssh:22 -N -T
